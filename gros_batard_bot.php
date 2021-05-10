006e4d0aa5ca9265d4d299cea52164b1219f944e
Simon <ferlat.simon@gmail.com>
2021-05-10 15:43:25 +0200

    reply to search results

--- gros_batard_bot.php
+++ gros_batard_bot.php
@@ -27,18 +27,23 @@ $search_metadata = $statuses->search_metadata;
 //reply to those tweets
 
 foreach ($tweets as $key => $value) {
+
+	//get handle
 	$handle = $value->user->screen_name;
 	$replytext = "@".$handle." Avec ceci ?";
 	print_r($replytext);
 	echo "<br>";
 
+	//get tweet ID
+	$tweetID = $value->id;
+	print_r($tweetID);
+	echo "<br>";
 
-/*
-$media = $connection->upload('media/upload', ['media' => "tarba.jpg"]);
-$mediaID =  $media->media_id_string;
+	//upload image
+	$media = $connection->upload('media/upload', ['media' => "tarba.jpg"]);
+	$mediaID =  $media->media_id_string;
 
-	
-$statues = $connection->post("statuses/update", ["status" => '@RockyStrongo Avec ceci ?', "in_reply_to_status_id" => "1200771462682095616", 'media_ids' => $mediaID]);
+	$statues = $connection->post("statuses/update", ["status" => $replytext, "in_reply_to_status_id" => $tweetID, 'media_ids' => $mediaID]);
 
 	if ($connection->getLastHttpCode() == 200)
 	{
@@ -50,27 +55,7 @@ $statues = $connection->post("statuses/update", ["status" => '@RockyStrongo Avec
 		$errortwitter = $connection->getLastHttpCode();
 		echo $errortwitter;
 	}
-*/
 
 }
 
-
-/*
-$media = $connection->upload('media/upload', ['media' => "tarba.jpg"]);
-$mediaID =  $media->media_id_string;
-
-
-	$statues = $connection->post("statuses/update", ["status" => '@RockyStrongo Avec ceci ?', "in_reply_to_status_id" => "1200771462682095616", 'media_ids' => $mediaID]);
-
-	if ($connection->getLastHttpCode() == 200)
-	{
-		echo "<br>Tweet posted succesfully";
-	}
-	else
-	{
-		echo "error posting tweet";
-		$errortwitter = $connection->getLastHttpCode();
-		echo $errortwitter;
-	}
-*/
 ?>
