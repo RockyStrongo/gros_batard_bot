b263c5a9ab2fba1cacd2848f98c6c2f74a5f8662
Simon <ferlat.simon@gmail.com>
2021-05-10 14:49:23 +0200

    search

--- gros_batard_bot.php
+++ gros_batard_bot.php
@@ -8,16 +8,23 @@ require "twitteroauth/autoload.php";
 
 use Abraham\TwitterOAuth\TwitterOAuth;
 
-
+//Connection
 $connection = new TwitterOAuth($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret);
 //increase timeout limit
 $connection->setTimeouts(10, 100);
 $content = $connection->get("account/verify_credentials");
 
+//search tweets containing "gros batard"
+$parameters = ["q" => '"gros batard"', "lang" => "fr", "count" => "100"];
 
+$statuses = $connection->get("search/tweets", $parameters);
+
+print("<pre>".print_r($statuses,true)."</pre>");
+
+//reply to those tweets
 
 /*
-	$statues = $connection->post("statuses/update", ["status" => 'hello world']);
+	$statues = $connection->post("statuses/update", ["status" => 'hi there @RockyStrongo', "in_reply_to_status_id" => "1200771462682095616"]);
 
 	if ($connection->getLastHttpCode() == 200)
 	{
@@ -30,5 +37,4 @@ $content = $connection->get("account/verify_credentials");
 		echo $errortwitter;
 	}
 */
-
 ?>
